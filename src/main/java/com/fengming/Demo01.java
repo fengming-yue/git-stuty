2e06c4328e7c6bb9330ecfd5efbeadb66e35ef1d
fengming-yue <2662997405@qq.com>
2022-08-12 23:13:40 +0800

    第一次提交!!!

---
+++ src/main/java/com/fengming/Demo01.java
@@ -0,0 +1,14 @@
+package com.fengming;
+
+/**
+ * @author FengMing
+ * @description
+ * @create 2022/08/12 23:10:42
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+    }
+}
